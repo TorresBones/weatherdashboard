5cb32b6e5fab7de3a3264e69fbec1c26a09ae13a
TorresBones <jonathantorres521@gmail.com>
2023-10-27 22:57:16 -0400

    Finishing and fixing script file

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,26 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 //  OpenWeather API
-    var apikey = dfeeac901eb9e6d9c301359f18f2b294;
+    var apiKey = 'dfeeac901eb9e6d9c301359f18f2b294';
 
 
-    var cityEl = $('h2#city');
-    var dateEl = $('h3#date');
-    var weatherIconEl = $('img#weather-icon');
-    var temperatureEl = $('span#temperature');
-    var humidity = $('span#humidity');
-    var windEl = $('span#win');
-    var uvIndexEl =$('span#uv-index');
+    var cityEl = $('#city');
+    var dateEl = $('#date');
+    var weatherIconEl = $('#weather-icon');
+    var temperatureEl = $('#temperature');
+    var humidityEl = $('#humidity');
+    var windEl = $('#win');
+    var uvIndexEl =$('#uv-index');
     var cityListEl = $('div.cityList');
 
     var cityInput = $('#city-input');
 
-    let pastCities = [];
+    var pastCities = [];
 
     function compare(a, b) {
         var cityA = a.city.toUpperCase();
         var cityB = b.city.toUpperCase();
 
-        let comparison = 0;
+        var comparison = 0;
         if (cityA > cityB) {
             comparison = 1;
         } else if (cityA < cityB) {
@@ -46,12 +46,12 @@ $(document).ready(function () {
     
     function buildURLFromInputs (city) {
         if (city) {
-            return 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}';
+            return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
         }
     }
 
     function buildURLFromId(id) {
-        return 'https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}';
+        return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
     }
 
     // Function to display the last 5 searched cities
@@ -59,11 +59,11 @@ $(document).ready(function () {
     function displayCities(pastCities) {
         cityListEl.empty();
         pastCities.splice(5);
-        let sortedCities = [...pastCities];
+        var sortedCities = [...pastCities];
         sortedCities.sort(compare);
         sortedCities.forEach(function (location) {
-            let cityDiv = $('<div>').addClass('col-12 city');
-            let cityBtn = $('<button').addClass('btn btn-light city-btn').text(location.city);
+            var cityDiv = $('<div>').addClass('col-12 city');
+            var cityBtn = $('<button').addClass('btn btn-light city-btn').text(location.city);
             cityDiv.append(cityBtn);
             cityListEl.append(cityDiv);
         });
@@ -93,8 +93,8 @@ $(document).ready(function () {
         }).then(function (response) {
 
             // Store current city in the past cities
-            let city = response.name;
-            let id = response.id;
+            var city = response.name;
+            var id = response.id;
             // Remove duplicate name cities
             if (pastCities[0]) {
                 pastCities = $.grep(pastCities, function (storedCities) {
@@ -107,16 +107,87 @@ $(document).ready(function () {
 
             // Display current weather in main DOM
             cityEl.text(response.name);
-            let formatedDate = moment.unix(response.dt).format('L');
+            var formatedDate = moment.unix(response.dt).format('L');
             dateEl.text(formatedDate);
-            let weatherIcon = response.weather[0].icon;
+            var weatherIcon = response.weather[0].icon;
             weatherIconEl.attr('src', `http://openweathermap.org/img/wn/${weatherIcon}.png`).attr('alt', response.weather[0].description);
             temperatureEl.html(((response.main.temp - 273.15) * 1.8 + 32).toFixed(1));
             humidityEl.text(reponse.main.humidity);
             windEl.text((response.wind.speed * 2.237).toFixed(1));
 
             // Call OpenWeather API with lat and lon to get UV index and the 5 day forecast
+
+            var latitude = response.coord.latitude;
+            var longitude = response.coord.longitude;
+            var queryURLAll = 'https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey';
+
+            $.ajax({
+                url: queryURLAll,
+                method: 'GET',
+            }).then(function (response) {
+                var uvIndex = response.current.uvi;
+                var uvColor = setUVIndexColor(uvIndex);
+                uvIndexEl.text(response.current.uvi);
+                uvIndexEl.attr('style', 'background-color: ${uvColor}; color ${uvColor === "yellow" ? "black" : "white"}');
+                var fiveDay = response.daily;
+
+                for (var i = 0; i < 5; i++) {
+                    var currentDay = fiveDay[1];
+                    $('div.day-${i} .card.title').text(moment.unix(currentDay.dt).format('L'));
+                    $('div.day-${i} .fiveDay-img').attr(
+                        src, 'http://openweathermap.org/img/wn/${currentDay.weather[0].icon}.png').attr('alt', currentDay.weather[0].description);
+                        $('div.day-${i} .fiveDay-temp').text(((currentDay.temp.day - 273.15) * 1.8 +32).toFixed(1));
+                        $('div.day-${i} .fiveDay-humid').text(currentDay.humidity);
+                }
+            });
         
-        })
+        });
     }
-})
+
+    // Function to display the last city searched in the search bar
+    function displayLastCitySearched () {
+        if (pastCities[0]) {
+            var queryURL = buildURLFromId(pastCities[0].id);
+            searchWeather(queryURL);
+        } else {
+            // If there is no past city searched load PA
+            var queryURL = buildURLFromInputs("Pennsylvania");
+            searchWeather(queryURL);
+        }
+    }
+
+    $('#search-btn').on('click', function (event) {
+        event.preventDefault();
+
+        var city = cityInput.val().trim();
+        city = encodeURIComponent(city);
+        
+        // city.replace('', '%20');
+
+        // Clear the input field
+        cityInput.val('');
+
+        if (city) {
+            var queryURL = buildURLFromInputs(city);
+            searchWeather(queryURL);
+        }
+    });
+
+    $(document).on('click', "button.city-btn", function (event) {
+        var clickedCity = $(this).text();
+        var foundCity = $.grep(pastCities, function (storedCities) {
+            return clickedCity === storedCities.city;
+        })
+
+        var queryURL = buildURLFromId(foundCity[0].id)
+        searchWeather(queryURL);
+    });
+
+    // Init for when page load
+
+    loadCities();
+    displayCities(pastCities);
+
+    displayLastCitySearched();
+
+// });
